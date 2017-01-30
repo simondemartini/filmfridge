b23dd444e4bbd9c17432d61b7a4abe1505d8de2a
Simon DeMartini <simondemartini@gmail.com>
2017-01-29 20:20:19 -0800

    Initial Empty Project

---
+++ app/src/main/java/edu/uw/tacoma/tcss450/team4/filmfridge/MainActivity.java
@@ -0,0 +1,13 @@
+package edu.uw.tacoma.tcss450.team4.filmfridge;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
