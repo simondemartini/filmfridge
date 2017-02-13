ca5614cf94dfd4a9f2589641ea14d2604fea26fe
Simon DeMartini <simondemartini@gmail.com>
2017-02-12 21:49:56 -0800

    Clean up javadoc

--- app/src/main/java/edu/uw/tacoma/tcss450/team4/filmfridge/FilmFragment.java
+++ app/src/main/java/edu/uw/tacoma/tcss450/team4/filmfridge/FilmFragment.java
@@ -24,8 +24,8 @@ import java.util.List;
 import edu.uw.tacoma.tcss450.team4.filmfridge.film.Film;
 
 /**
- * A fragment representing a list of Items.
- * <p/>
+ * A fragment representing a list of Films.
+ *
  * Activities containing this fragment MUST implement the {@link OnListFragmentInteractionListener}
  * interface.
  */
