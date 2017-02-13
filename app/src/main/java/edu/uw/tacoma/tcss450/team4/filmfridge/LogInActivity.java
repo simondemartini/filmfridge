a5cd5f67260cc2cc2f1972b756bb5bca970025fb
Samantha Ong <sjfoh@uw.edu>
2017-02-12 17:23:50 -0800

    login screen skeleton

--- app/src/main/java/edu/uw/tacoma/tcss450/team4/filmfridge/LogInActivity.java
+++ app/src/main/java/edu/uw/tacoma/tcss450/team4/filmfridge/LogInActivity.java
@@ -3,11 +3,11 @@ package edu.uw.tacoma.tcss450.team4.filmfridge;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class LogInActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
     }
 }
