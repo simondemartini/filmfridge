9cc1690d632f53fdb579c186697a6ae7729c604b
Simon DeMartini <simondemartini@gmail.com>
2017-02-24 15:02:27 -0800

    Revert "Remove TODOs"
    This reverts commit 8ef3db78c43619e0cb7387882974fc717fcb6123.

--- app/src/main/java/edu/uw/tacoma/tcss450/team4/filmfridge/FilmFragment.java
+++ app/src/main/java/edu/uw/tacoma/tcss450/team4/filmfridge/FilmFragment.java
@@ -136,6 +136,8 @@ public class FilmFragment extends Fragment {
             }
         }
 
+        //TODO: Add progress spinner or something
+
         @Override
         protected void onPostExecute(List<Film> result) {
             // Everything is good, show the list.
