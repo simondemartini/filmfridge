2e6fa4b992ed234d1b6421d27c7a842116af1962
Simon DeMartini <simondemartini@gmail.com>
2017-02-28 23:30:19 -0800

    Barebones drawer layout

--- app/src/main/java/edu/uw/tacoma/tcss450/team4/filmfridge/FilmActivity.java
+++ app/src/main/java/edu/uw/tacoma/tcss450/team4/filmfridge/FilmActivity.java
@@ -3,13 +3,20 @@ package edu.uw.tacoma.tcss450.team4.filmfridge;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.res.Configuration;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.SystemClock;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import edu.uw.tacoma.tcss450.team4.filmfridge.authenticate.SignInActivity;
@@ -19,14 +26,27 @@ public class FilmActivity extends AppCompatActivity implements
     UpcomingListFragment.OnListFragmentInteractionListener,
         FilmDetailFragment.OnDetailFragmentInteractionListener {
 
-    private FilmDetailFragment mFilmDetailFragment;
+    private ListView mDrawerList;
+    private ActionBarDrawerToggle mDrawerToggle;
+    private DrawerLayout mDrawerLayout;
+    private String mActivityTitle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_films);
+        setContentView(R.layout.activity_drawer_layout);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        mDrawerList = (ListView) findViewById(R.id.left_drawer);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mActivityTitle = getTitle().toString();
+        if(getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+        getSupportActionBar().setHomeButtonEnabled(true);
+        addDrawerItems();
+        setupDrawer();
 
         if (savedInstanceState == null || getSupportFragmentManager().findFragmentById(R.id.list) == null) {
             UpcomingListFragment courseFragment = new UpcomingListFragment();
@@ -36,15 +56,75 @@ public class FilmActivity extends AppCompatActivity implements
         }
     }
 
+    /**
+     * After onCreate. Update some things
+     * @param savedInstanceState saved state
+     */
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        //Update drawer toggle icon
+        mDrawerToggle.syncState();
+    }
+
+    /**
+     * Activityi changes such as oritnetation changes, or showing keyboard
+     * @param newConfig new config
+     */
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        //update drawer toggle
+        mDrawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    /**
+     * A helper method to populate the nav drawer with options
+     */
+    private void addDrawerItems() {
+        String[] nav = {"Upcoming", "My List", "Settings"};
+        ArrayAdapter<String> mAdapter = new ArrayAdapter<String>(this, R.layout.drawer_list_item, nav);
+        mDrawerList.setAdapter(mAdapter);
+        mDrawerList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(FilmActivity.this, "Time for an upgrade!", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    /**
+     * Create the nav drawer.
+     */
+    private void setupDrawer() {
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout,
+                R.string.drawer_open, R.string.drawer_close) {
+
+            /** Called when a drawer has settled in a completely open state. */
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+
+            /** Called when a drawer has settled in a completely closed state. */
+            public void onDrawerClosed(View view) {
+                super.onDrawerClosed(view);
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+        };
+        mDrawerToggle.setDrawerIndicatorEnabled(true);
+        mDrawerLayout.addDrawerListener(mDrawerToggle);
+    }
+
     @Override
     public void onListFragmentInteraction(Film item) {
-        mFilmDetailFragment = new FilmDetailFragment();
+        FilmDetailFragment filmDetailFragment = new FilmDetailFragment();
         Bundle args = new Bundle();
         args.putSerializable(FilmDetailFragment.FILM_ITEM_SELECTED, item);
-        mFilmDetailFragment.setArguments(args);
+        filmDetailFragment.setArguments(args);
 
         getSupportFragmentManager().beginTransaction()
-                .replace(R.id.film_fragment_container, mFilmDetailFragment)
+                .replace(R.id.film_fragment_container, filmDetailFragment)
                 .addToBackStack(null)
                 .commit();
     }
@@ -76,6 +156,8 @@ public class FilmActivity extends AppCompatActivity implements
         } else if(id == R.id.action_share) {
             //implemented in the fragment
             return false;
+        } else if (mDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
         }
 
         return super.onOptionsItemSelected(item);
