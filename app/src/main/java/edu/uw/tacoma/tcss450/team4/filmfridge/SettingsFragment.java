e8d8d7a416271dabb310b70db41120cc1086af56
Samantha Ong <sjfoh@uw.edu>
2017-03-07 17:25:09 -0800

    Added SettingsFragment

--- app/src/main/java/edu/uw/tacoma/tcss450/team4/filmfridge/SettingsFragment.java
+++ app/src/main/java/edu/uw/tacoma/tcss450/team4/filmfridge/SettingsFragment.java
@@ -8,6 +8,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.NumberPicker;
+import android.widget.TextView;
 
 
 /**
@@ -66,10 +67,10 @@ public class SettingsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-
-
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_settings, container, false);
+        View v = inflater.inflate(R.layout.fragment_settings, container, false);
+
+        return v;
     }
 
     // TODO: Rename method, update argument and hook method into UI event
